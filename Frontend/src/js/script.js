268b222e182525922193ea7f66812fa39807bcf6
Robelmuluwork <robelmuluwork91@gmail.com>
2024-11-10 00:57:26 +0300

    minor updates

--- Frontend/src/js/script.js
+++ Frontend/src/js/script.js
@@ -16,3 +16,36 @@ document.addEventListener("DOMContentLoaded", function () {
     registerForm.classList.remove("hidden");
   });
 });
+
+//change words in home page
+const words = ["ideas", "knowledge", "learning"];
+const colors = ["text-blue-600", "text-green-600", "text-purple-600"];
+let currentIndex = 0;
+
+function changeWord() {
+  const animatedWord = document.getElementById("animated-word");
+  animatedWord.classList.remove(colors[currentIndex]);
+  currentIndex = (currentIndex + 1) % words.length;
+  animatedWord.textContent = words[currentIndex];
+  animatedWord.classList.add(colors[currentIndex]);
+}
+
+setInterval(changeWord, 2000);
+
+// Profile dropdown functionality
+const profileButton = document.getElementById("profileButton");
+const profileMenu = document.getElementById("profileMenu");
+
+profileButton.addEventListener("click", () => {
+  profileMenu.classList.toggle("hidden");
+});
+
+// Close dropdown when clicking outside
+document.addEventListener("click", (event) => {
+  if (
+    !profileButton.contains(event.target) &&
+    !profileMenu.contains(event.target)
+  ) {
+    profileMenu.classList.add("hidden");
+  }
+});
